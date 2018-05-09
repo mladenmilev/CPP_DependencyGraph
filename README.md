e79d1025d8ca9066357b3233e6a9bda3d19d174e
Mladen Milev <mladen.milev@coriant.com>
2018-05-09 10:38:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Simplistic tool to extract *#include* project dependencies coming from other projects.
+
+Relies on particular MS Visual Studio setup
+Identifies dependent include files based on regular expression.
+
+Install Plotly - https://plot.ly/python/getting-started/
+$ pip install plotly
+
+Install Networkx to handle graphs - https://networkx.github.io/documentation/stable/install.html
+$ pip install networkx
---
+++ main.py
@@ -0,0 +1,47 @@
+import os, fnmatch, re
+
+path = 'C:\\_src\\7196\\'
+projects = ['BMesh', 'BRing', 'Equipment', 'utilities', 'XData', 'XMesh', 'XN', 'XORA', 'XRing', 'Xs']
+pattern_cpp = "*.cpp"
+pattern_h = "*.h"
+
+result_matrix = {}
+
+
+def print_info():
+    for project,file_matrix in result_matrix.iteritems():
+        print ("===================================================")
+        print ("I                                                  ")
+        print ("I PROJECT: \t" + project)
+        print ("I___________________________________________________")
+        for file,lines  in file_matrix.iteritems():
+            print ("I")
+            print ("I FILE: " + file)
+            for line in lines:
+                print ("I   " + line)
+        print ("I                                                  ")
+        print ("===================================================")
+
+for prj in projects:
+    p_path = path + prj + "\\"
+
+    listOfFiles = os.listdir(p_path)
+
+    file_matrix = {}
+    for file_name in listOfFiles:
+        if fnmatch.fnmatch(file_name, pattern_cpp) or fnmatch.fnmatch(file_name, pattern_h):
+            inputfile = open(p_path + file_name)
+            for line in inputfile:
+                result = re.match('#include \"\../.{1,}\.h', line)
+                if result:
+                    if file_name not in file_matrix:
+                        file_matrix[file_name] = [line.rstrip()]
+                    else:
+                        file_matrix[file_name].append(line.rstrip())
+            inputfile.close()
+
+    if len(file_matrix) != 0:
+        result_matrix[prj] = file_matrix
+
+print_info()
+
